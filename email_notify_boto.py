7ccaae91719a763603078a871a73fa2321841680
Ubuntu <ubuntu@ip-172-31-26-160.us-west-2.compute.internal>
2018-07-24 12:05:21 +0000

    add amazon emailer

--- email_notify_boto.py
+++ email_notify_boto.py
@@ -5,13 +5,12 @@ from bs4 import BeautifulSoup
 #Next, log in to the server
 import boto3
 
-def send_email():
 from botocore.exceptions import ClientError
 
 # Replace sender@example.com with your "From" address.
 # This address must be verified with Amazon SES.
 SENDER = "sdhains2@gmail.com"
-
+import time 
 # Replace recipient@example.com with a "To" address. If your account
 # is still in the sandbox, this address must be verified.
 RECIPIENT = "sam@samhains.com"
@@ -37,11 +36,6 @@ BODY_TEXT = ("Amazon SES Test (Python)\r\n"
 BODY_HTML = """<html>
 <head></head>
 <body>
-  <h1>Amazon SES Test (SDK for Python)</h1>
-  <p>This email was sent with
-    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
-    <a href='https://aws.amazon.com/sdk-for-python/'>
-      AWS SDK for Python (Boto)</a>.</p>
 </body>
 </html>
             """
@@ -52,54 +46,61 @@ CHARSET = "UTF-8"
 # Create a new SES resource and specify a region.
 client = boto3.client('ses',region_name=AWS_REGION)
 
+def send_email(body_text, subject):
 # Try to send the email.
-try:
-    #Provide the contents of the email.
-    response = client.send_email(
-        Destination={
-            'ToAddresses': [
-                RECIPIENT,
-            ],
-        },
-        Message={
-            'Body': {
-                'Html': {
-                    'Charset': CHARSET,
-                    'Data': BODY_HTML,
-                },
-                'Text': {
-                    'Charset': CHARSET,
-                    'Data': BODY_TEXT,
-                },
-            },
-            'Subject': {
-                'Charset': CHARSET,
-                'Data': SUBJECT,
-            },
-        },
-        Source=SENDER,
-        # If you are not using a configuration set, comment or delete the
-        # following line
-        # ConfigurationSetName=CONFIGURATION_SET,
-    )
-# Display an error if something goes wrong.
-except ClientError as e:
-    print(e.response['Error']['Message'])
-else:
-    print("Email sent! Message ID:"),
-    print(response['MessageId'])
+	try:
+	    #Provide the contents of the email.
+	    response = client.send_email(
+		Destination={
+		    'ToAddresses': [
+			RECIPIENT,
+		    ],
+		},
+		Message={
+		    'Body': {
+			'Html': {
+			    'Charset': CHARSET,
+			    'Data': BODY_HTML,
+			},
+			'Text': {
+			    'Charset': CHARSET,
+			    'Data': body_text,
+			},
+		    },
+		    'Subject': {
+			'Charset': CHARSET,
+			'Data': subject,
+		    },
+		},
+		Source=SENDER,
+		# If you are not using a configuration set, comment or delete the
+		# following line
+		# ConfigurationSetName=CONFIGURATION_SET,
+	    )
+	# Display an error if something goes wrong.
+	except ClientError as e:
+	    print(e.response['Error']['Message'])
+	else:
+	    print("Email sent! Message ID:"),
+	    print(response['MessageId'])
 
-# def check_nyu():
-#     url = "https://m.albert.nyu.edu/app/catalog/classsection/NYUNV/1188/22604"
-#
-#     r  = requests.get(url)
-#
-#     data = r.text
-#
-#     soup = BeautifulSoup(data, "html.parser")
-#
-#     for link in soup.find_all(text="Closed"):
-#         if link == "Closed":
-#         else:
-#
-# check_nyu()
+def check_nyu():
+    url = "https://m.albert.nyu.edu/app/catalog/classsection/NYUNV/1188/22604"
+
+    r  = requests.get(url)
+
+    data = r.text
+
+    soup = BeautifulSoup(data, "html.parser")
+
+    for link in soup.find_all(text="Closed"):
+        if link == "Closed":
+		timestr = time.strftime("%Y%m%d-%H%M%S") 
+		with open("/home/ubuntu/Code/nyu_scraper/test.txt", "a") as myfile:     
+			myfile.write("{} : CLOSED\n".format(timestr))
+        else:
+		with open("/home/ubuntu/Code/nyu_scraper/test.txt", "a") as myfile:     
+			myfile.write("{} : OPEN\n".format(timestr))
+		send_email("waitlist open", "waitlist open")
+
+check_nyu()
